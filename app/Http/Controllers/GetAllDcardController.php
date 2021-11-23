4eedad2da78325e5c4bde5db83038defa5b4e334
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2021-11-23 10:18:42 +0800

    add getAllDcard

---
+++ app/Http/Controllers/GetAllDcardController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GetAllDcard;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Response;
+
+class GetAllDcardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $dcardAll = DB::table('dcard_rawdata')
+        ->leftJoin('nlp_analysis', 'dcard_rawdata.Id', '=', 'nlp_analysis.Id')
+        ->leftJoin('comparison', 'comparison.Id', '=', 'nlp_analysis.Id')
+        ->select('dcard_rawdata.Id', 'dcard_rawdata.Title', 'dcard_rawdata.CreatedAt', 'dcard_rawdata.Content'
+        , 'nlp_analysis.SA_Score', 'nlp_analysis.SA_Class', 'comparison.Level', 'comparison.KeywordLevel1', 
+        'comparison.KeywordLevel2', 'comparison.KeywordLevel3')
+        ->orderByDesc('dcard_rawdata.Id')
+        ->get();
+
+        return response()->json($dcardAll, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+
+        // return \Response::json($dcardAll, Response::HTTP_OK);//
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GetAllDcard  $getAllDcard
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GetAllDcard $getAllDcard)
+    {
+        return response($dcardTest, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GetAllDcard  $getAllDcard
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GetAllDcard $getAllDcard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GetAllDcard  $getAllDcard
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GetAllDcard $getAllDcard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GetAllDcard  $getAllDcard
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GetAllDcard $getAllDcard)
+    {
+        //
+    }
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -41,10 +41,12 @@ class RouteServiceProvider extends ServiceProvider
             Route::prefix('api')
                 ->middleware('api')
                 ->namespace($this->namespace)
+                ->namespace('App\Http\Controllers')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)
+                ->namespace('App\Http\Controllers')
                 ->group(base_path('routes/web.php'));
         });
     }
