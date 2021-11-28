f4ca311d67942740ad2037cb41de2ea710362a07
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2021-11-28 22:18:05 +0800

    coding json format

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,10 +21,10 @@ class UserController extends Controller
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
             $success =  $user->createToken('MyApp')->accessToken;
-            return response()->json(['success' => $success], $this->successStatus);
+            return response()->json(['message' => $success], $this->successStatus);
         }
-        else{
-            return response()->json(['error'=>'Unauthorised'], 401);
+        else {
+            return response()->json(['message' => 'Unauthorised'], 401);
         }
     }
 
