4ef0151d8ffb5e034c3302cade5c05ef42c2b584
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-02-02 19:42:22 +0800

    testing nginx

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-service nginx start
-php-fpm
+sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
+
+php-fpm -D
+
+while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
+
+nginx
