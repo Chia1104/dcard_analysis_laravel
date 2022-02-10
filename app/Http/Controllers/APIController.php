105719754e5e4da0e6f515e5db797689f77fc65b
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-02-11 02:08:00 +0800

    add auth:api on api request

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,10 +10,10 @@ use mysql_xdevapi\Exception;
 class APIController extends Controller
 {
 
-//    public function __construct()
-//    {
-//        $this->middleware('auth:api');
-//    }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
 
     /**
      * Display a listing of the resource.
