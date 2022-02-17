27bcb54be4176847b19723aac3998fdd93ac1b21
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-02-17 20:28:45 +0800

    add paginate(30) to respond

--- app/Http/Controllers/APIV2Controller.php
+++ app/Http/Controllers/APIV2Controller.php
@@ -27,48 +27,11 @@ class APIV2Controller extends Controller
     public function getDcards(Request $request): JsonResponse
     {
         try {
-            $limit = $request -> limit == null ? 10 : $request -> limit;
-            if ($limit <= 0) {
-                $limit = 1;
-            }
 
             $dcards = Dcard::with(['nlp', 'comparison'])
                 ->main()
-                ->limit($limit)
-                ->get();
+                ->paginate(30);
 
-            return response()->json($dcards, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
-                JSON_UNESCAPED_UNICODE);
-        } catch (\Exception $e) {
-            $error['message'] = '404 Not Found';
-            return response()->json($error, 404);
-        }
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @param Request $request
-     * @return JsonResponse
-     */
-
-    public function beforeId(Request $request): JsonResponse {
-
-        try {
-            $limit = $request -> limit == null ? 10 : $request -> limit;
-            $beforeId = $request -> beforeId;
-            if ($limit <= 0) {
-                $limit = 1;
-            } elseif ($beforeId == null) {
-                $error['message'] = '404 Not Found';
-                return response()->json($error, 404);
-            }
-
-            $dcards = Dcard::with(['nlp', 'comparison'])
-                ->main()
-                ->where('Id', '<', $beforeId)
-                ->limit($limit)
-                ->get();
             return response()->json($dcards, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
                 JSON_UNESCAPED_UNICODE);
         } catch (\Exception $e) {
@@ -90,7 +53,7 @@ class APIV2Controller extends Controller
             if($request->has('search')){
                 $search = $request -> search;
                 $dcards = Dcard::search($search)
-                    ->get();
+                    ->paginate(30);
 
                 if ($dcards->isEmpty()) {
                     $error['message'] = '404 Not Found';
@@ -147,10 +110,11 @@ class APIV2Controller extends Controller
     public function getDateBetween($date1, $date2): JsonResponse
     {
         try {
+            $date2 = $date2 . 'T23:59:59';
             $dcards = Dcard::with(['nlp', 'comparison'])
                 ->main()
                 ->whereBetween('CreatedAt', [$date1, $date2])
-                ->get();
+                ->paginate(30);
 
             if ($dcards->isEmpty()) {
                 $error['message'] = '404 Not Found';
@@ -169,25 +133,32 @@ class APIV2Controller extends Controller
      * Display a listing of the resource.
      *
      *
-     * @param Request $request
+     * @param $type
      * @return JsonResponse
      */
-    public function getDateDcards(Request $request): JsonResponse
+    public function getDateDcards($type): JsonResponse
     {
-        $type = $request -> type == null ? 'today' : $request -> type;
 
         try {
+//            $today = Carbon::now();
+//            $tomorrow = Carbon::now()->addDay();
+            $today = "2021-11-09";
+            $tomorrow = "2021-11-10";
+//            $day = date('w');
+//            $week_start = date('Y-m-d', strtotime('-'.$day.' days'));
+//            $week_end = date('Y-m-d', strtotime('+'.(6-$day).' days'));
+            $week_start = "2021-11-07";
+            $week_end = "2021-11-14";
+//            $m0d1 = date("Y-m-d", strtotime("first day of 0 month"));
+//            $m0d31 = date("Y-m-d", strtotime("last day of 0 month"));
+            $m0d1 = "2021-11-01";
+            $m0d31 = "2021-12-01";
             switch ($type) {
                 case 'today':
-//                    $today = date("Y-m-d");
-//                    $tomorrow = Carbon::now()->addDay();
-                    $today = "2021-11-09";
-                    $tomorrow = "2021-11-10";
                     $dcards = Dcard::with(['nlp', 'comparison'])
                         ->main()
                         ->whereBetween('CreatedAt', [$today, $tomorrow])
-//                        ->whereDate('CreatedAt', $today)
-                        ->get();
+                        ->paginate(30);
                     if ($dcards->isEmpty()) {
                         $error['message'] = '404 Not Found';
                         return response()->json($error, 404);
@@ -196,15 +167,10 @@ class APIV2Controller extends Controller
                     return response()->json($dcards, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
                         JSON_UNESCAPED_UNICODE);
                 case 'week':
-//                    $day = date('w');
-//                    $week_start = date('Y-m-d', strtotime('-'.$day.' days'));
-//                    $week_end = date('Y-m-d', strtotime('+'.(6-$day).' days'));
-                    $week_start = "2021-11-07";
-                    $week_end = "2021-11-13";
                     $dcards = Dcard::with(['nlp', 'comparison'])
                         ->main()
                         ->whereBetween('CreatedAt', [$week_start, $week_end])
-                        ->get();
+                        ->paginate(30);
                     if ($dcards->isEmpty()) {
                         $error['message'] = '404 Not Found';
                         return response()->json($error, 404);
@@ -213,14 +179,10 @@ class APIV2Controller extends Controller
                     return response()->json($dcards, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
                         JSON_UNESCAPED_UNICODE);
                 case 'month':
-//                    $m0d1 = date("Y-m-d", strtotime("first day of 0 month"));
-//                    $m0d31 = date("Y-m-d", strtotime("last day of 0 month"));
-                    $m0d1 = "2021-11-01";
-                    $m0d31 = "2021-11-30";
                     $dcards = Dcard::with(['nlp', 'comparison'])
                         ->main()
                         ->whereBetween('CreatedAt', [$m0d1, $m0d31])
-                        ->get();
+                        ->paginate(30);
                     if ($dcards->isEmpty()) {
                         $error['message'] = '404 Not Found';
                         return response()->json($error, 404);
@@ -232,8 +194,8 @@ class APIV2Controller extends Controller
                 default:
                     $dcards = Dcard::with(['nlp', 'comparison'])
                         ->main()
-                        ->whereDate('CreatedAt', $type)
-                        ->get();
+                        ->whereDate('CreatedAt', [$today, $tomorrow])
+                        ->paginate(30);
                     if ($dcards->isEmpty()) {
                         $error['message'] = '404 Not Found';
                         return response()->json($error, 404);
@@ -256,12 +218,12 @@ class APIV2Controller extends Controller
      * @param Request $request
      * @return JsonResponse
      */
-    public function getPieChartDatas(Request $request): JsonResponse
+    public function getTotalSAClass(Request $request): JsonResponse
     {
         try {
             $date1 = $request -> date1;
             $date2 = $request -> date2;
-
+            $date2 = $date2 . 'T23:59:59';
             $saclass = array(0 => 'Positive', 1 => 'Neutral', 2 => 'Negative');
             $pdatas = array();
 
@@ -290,17 +252,17 @@ class APIV2Controller extends Controller
      * @param Request $request
      * @return JsonResponse
      */
-    public function getLineChartDatas(Request $request): JsonResponse
+    public function getAvgSAScore(Request $request): JsonResponse
     {
         try {
             $date1 = $request -> date1;
             $date2 = $request -> date2;
-            $m0d31 = "2021-11-30";
-            $m11d1 = "2020-12-01";
+            $date2 = $date2 . 'T23:59:59';
             $avg = Nlp::main()
                 ->whereBetween('CreatedAt', [$date1, $date2])
                 ->avg('SA_Score');
-            return response()->json($avg, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            $result['avg'] = $avg;
+            return response()->json($result, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
                 JSON_UNESCAPED_UNICODE);
         } catch (\Exception $e) {
             $error['message'] = '404 Not Found' . $e;
